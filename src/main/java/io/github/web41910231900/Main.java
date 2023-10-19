631c4ca1a15f7ad6eb9e09529568845bee5cb436
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-19 15:47:19 +0300

    feat: initial commit

---
+++ src/main/java/io/github/web41910231900/Main.java
@@ -0,0 +1,8 @@
+package io.github.web41910231900;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // what??
+        // girls sometimes may be beautiful...
+    }
+}
