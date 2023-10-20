4db9377884d72d5b241349e981cd662f4ae011ba
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-21 00:54:05 +0300

    feat: hello world on spring rest api

---
+++ src/main/java/io/github/web41910231900/configuration/AppConfig.java
@@ -0,0 +1,13 @@
+package io.github.web41910231900.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("io.github.web41910231900")
+public class AppConfig implements WebMvcConfigurer {
+
+}
---
+++ src/main/java/io/github/web41910231900/model/Greeting.java
@@ -0,0 +1,5 @@
+package io.github.web41910231900.model;
+
+public record Greeting(long id, String content) {
+
+}
