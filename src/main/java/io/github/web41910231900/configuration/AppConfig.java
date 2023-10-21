303c563a9c4e7e1de6fc414cf9dfca8d12e630f1
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-22 00:40:52 +0300

    fix: cross-origin in rest

--- src/main/java/io/github/web41910231900/configuration/AppConfig.java
+++ src/main/java/io/github/web41910231900/configuration/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -13,7 +14,15 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @ComponentScan("io.github.web41910231900")
 public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/app/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+        @Bean
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
         return new MvcRequestMatcher.Builder(introspector);
     }
