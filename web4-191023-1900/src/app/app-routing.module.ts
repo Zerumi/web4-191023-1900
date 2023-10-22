5b65c6043f3ba9eda0c8c6a25b37eb841e2ca5e0
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-22 17:27:55 +0300

    feat: front from 3rd lab

--- web4-191023-1900/src/app/app-routing.module.ts
+++ web4-191023-1900/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataComponent } from "./data/data.component";
+import {MainComponent} from "./main/main.component";
 
 // making (связывание)
 const routes: Routes = [
   {
     path: '',
     component: DataComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
   }
 ];
 
