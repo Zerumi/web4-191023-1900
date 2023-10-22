0bf0bf011e31b7fc167e7ef9897a6827b236d60d
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-22 16:46:13 +0300

    feat: primeng button example

--- web4-191023-1900/src/app/app.module.ts
+++ web4-191023-1900/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
 import {HttpClientModule} from "@angular/common/http";
+import { ButtonModule } from 'primeng/button';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataComponent
+    DataComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
