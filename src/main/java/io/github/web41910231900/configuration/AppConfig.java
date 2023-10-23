1946edae4de99d587114ba680bfe5b98e8cb2ec0
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-23 03:48:22 +0300

    fix: missing beans in security config

--- src/main/java/io/github/web41910231900/configuration/AppConfig.java
+++ src/main/java/io/github/web41910231900/configuration/AppConfig.java
@@ -3,6 +3,7 @@ package io.github.web41910231900.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -22,8 +23,13 @@ public class AppConfig implements WebMvcConfigurer {
                 .allowCredentials(true).maxAge(3600);
     }
 
-        @Bean
-    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+    @Bean
+    public MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
         return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
