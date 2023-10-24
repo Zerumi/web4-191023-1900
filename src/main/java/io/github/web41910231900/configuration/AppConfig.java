ac60fb7e0e9632d94e34efedc7bb475d54eba7d9
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-24 05:35:49 +0300

    fix: apply datetime from backend

--- src/main/java/io/github/web41910231900/configuration/AppConfig.java
+++ src/main/java/io/github/web41910231900/configuration/AppConfig.java
@@ -1,14 +1,21 @@
 package io.github.web41910231900.configuration;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -22,6 +29,16 @@ public class AppConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true).maxAge(3600);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        WebMvcConfigurer.super.extendMessageConverters(converters);
+        converters.add(new MappingJackson2HttpMessageConverter(
+                new Jackson2ObjectMapperBuilder()
+                        .dateFormat(new StdDateFormat())
+                        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                        .build()));
     }
 
     @Bean
